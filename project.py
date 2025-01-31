50c758237ccab7db678c26be5ccfead4755f7098
Danilo-Jeffry <d.m.yakovliev@student.khai.edu>
2025-01-31 22:31:59 +0200

    added new commands, updated code

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 
 conn = sqlite3.connect('expenses.db')
 cursor = conn.cursor()
@@ -6,17 +7,51 @@ cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS expenses
                   (id INTEGER PRIMARY KEY, amount REAL, category TEXT, date TEXT)''')
 
-def add_expense(amount, category, date):
+def add_expense(info):
+
+    info = str.split(info, " ")
+    amount = info[0]
+    category = info[1]
+    date = info[2]
+
     cursor.execute('''INSERT INTO expenses (amount, category, date)
                       VALUES (?, ?, ?)''', (amount, category, date))
     conn.commit()
 
-add_expense(50.0, 'їжа', '2025-01-30')
-add_expense(20.0, 'транспорт', '2025-01-31')
 
-cursor.execute('''SELECT * FROM expenses''')
-expense = cursor.fetchall()
-for i in expense:
-    print(i)
-# Закриття з'єднання з базою даних
+def all_expenses():
+
+    cursor.execute('''SELECT * FROM expenses''')
+    expense = cursor.fetchall()
+    for i in expense:
+        print(i)
+
+
+os.system("cls")
+
+while True:
+
+    x = input(str("Що ви хочете зробити?\n1 - Додати витрати\n2 - відобразити всі витрати\n9 - вийти\n0 - очистити таблицю\n"))
+
+    if x == '1':
+        a = input("Введіть ваши витрати: (Кількість, куди саме, дату у формати YYYY-MM-DD)")
+        add_expense(a)
+        os.system('cls')
+
+    elif x == '2':
+        all_expenses()
+
+    elif x == "9":
+        break
+
+    elif x == "0":
+
+        print("Ви в цьому впевнені? (y - так, n - ні)")
+        answer = input()
+
+        if answer == 'y':
+            cursor.execute("""DELETE * FROM expenses""")
+        if answer == 'n':
+            pass
+
 cursor.close()
